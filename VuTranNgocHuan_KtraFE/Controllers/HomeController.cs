e66cc155d60738c62840ed06b59220301acd21d4
Vũ Trần Ngọc Huân <23DH111160@st.huflit.edu.vn>
2024-10-08 11:35:55 +0700

    Add project files.

---
+++ VuTranNgocHuan_KtraFE/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VuTranNgocHuan_KtraFE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ VuTranNgocHuan_KtraFE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VuTranNgocHuan_KtraFE.MvcApplication" Language="C#" %>
